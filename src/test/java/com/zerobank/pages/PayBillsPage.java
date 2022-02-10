c05496031fb261601bade578a47574092e299d89
Melek <hmlkshn@gmail.com>
2022-02-10 14:19:44 +0300

    add locators and methods

--- src/test/java/com/zerobank/pages/PayBillsPage.java
+++ src/test/java/com/zerobank/pages/PayBillsPage.java
@@ -3,8 +3,21 @@ package com.zerobank.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class PayBillsPage extends BasePage{
 
     @FindBy(partialLinkText = "Add New Payee")
     public WebElement addNewPayeeLink;
+
+    @FindBy(xpath = "(//div[@class='board-content'])[2]//div[@class='control-group']")
+    public List<WebElement> inputBoxes;
+
+
+
+    @FindBy(xpath = "//button[@data-dismiss='alert']/../div[@id='alert_content']")
+    public WebElement message;
+
+
+
 }
--- src/test/java/com/zerobank/stepDefinitions/AddNewPayeeStepDef.java
+++ src/test/java/com/zerobank/stepDefinitions/AddNewPayeeStepDef.java
@@ -1,4 +1,32 @@
 package com.zerobank.stepDefinitions;
 
+import com.zerobank.pages.PayBillsPage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+import java.util.Map;
+
 public class AddNewPayeeStepDef {
+    PayBillsPage page = new PayBillsPage();
+
+    @Given("Add New Payee tab")
+    public void add_New_Payee_tab() {
+        page.addNewPayeeLink.click();
+    }
+
+    @And("create new payee using following information")
+    public void create_new_payee_using_following_information(Map<String,String> expectedInputs) {
+
+
+
+
+    }
+
+    @Then("message {string} should be displayed")
+    public void message_should_be_displayed(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }
